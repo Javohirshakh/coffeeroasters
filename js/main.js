a6297a5c997cefa8872088a8b912ed92e5379aec
Javohirshakh <rajabov4you@gmail.com>
2022-03-17 01:00:55 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var elSiteHeaderSection = document.querySelector(".site-header");
+var elSiteHeaderToggler = elSiteHeaderSection.querySelector(".js-site-header-toggler");
+
+if (elSiteHeaderToggler) {
+  elSiteHeaderToggler.addEventListener("click", function () {
+    elSiteHeaderSection.classList.toggle("site-header--open");
+  });
+}
+
+
+
+var elsOrderSection = document.querySelectorAll(".order-section");
+var elsOrderButtonToggler = document.querySelectorAll('.order-section-toggler');
+
+elsOrderButtonToggler.forEach( function (button) {
+  button.addEventListener('click', function () {
+    button.closest('.order-section').classList.toggle('order-section--active');
+
+  })
+})
+
+
+var elBtnCreatePlan = document.querySelector(".btn-create");
+var elOrderModal = document.querySelector('.order-modal');
+elBtnCreatePlan.addEventListener("click", function () {
+  elOrderModal.classList.add('order-modal--active');
+})
+
+var elOrderModalActiveSection = document.querySelector(".order-modal--active");
+
+
+elOrderModal.addEventListener("click", function () {
+    elOrderModal.classList.remove("order-modal--active");
+})
+
+
